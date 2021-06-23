762dcfee5bd43202c0c9577a3b24d9bb20f63593
Joey Paris <joey@leadjig.com>
2021-06-23 14:37:19 -0400

    Working version with 100% test coverage

--- lib/race_block.rb
+++ lib/race_block.rb
@@ -1,8 +1,87 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+require 'logger'
+require 'redis'
 require_relative "race_block/version"
 
 module RaceBlock
   class Error < StandardError; end
-  # Your code goes here...
+  
+  def self.client(reload=false)
+    if @redis.nil? || reload
+      @redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+
+      begin
+          @redis.ping
+      rescue Redis::CannotConnectError => e
+          RaceBlock.logger.error e
+      end
+    end
+    @redis
+  end
+
+  def self.logger
+    @logger ||= Logger.new(STDOUT)
+  end
+
+  def self.key key
+    "race_block_#{key}"
+  end
+
+  def self.reset key
+    RaceBlock.client.del(RaceBlock.key(key))
+  end
+
+  def self.start key, sleep_delay: 0.5, expire: 60, expire_immediately: false, debug: false, desync_tokens: false
+    raise('A key must be provided to start a RaceBlock') if key.nil? || key.empty?
+
+    @key = RaceBlock.key(key)
+
+    # Set an expiration for the token if the key is defined but doesn't
+    # have an expiration set (happens sometimes if a thread dies early).
+    # `-1` means the key is set but does not expire, `-2` means the key is
+    # not set
+    RaceBlock.client.expire( @key, 10 ) if -1 == RaceBlock.client.ttl( @key )
+
+    if !RaceBlock.client.get( @key )
+      sleep rand(0.0..sleep_delay) if desync_tokens
+      token = SecureRandom.hex
+      RaceBlock.client.set( @key, token)
+      RaceBlock.client.expire(@key, 15)
+      sleep sleep_delay
+      # Okay, so I feel like this is pseudo science, but whatever. Our
+      # race condition comes from when the same cron job is called by
+      # several different server instances at the same time
+      # (theoretically) all within the same second (much less really).
+      # By waiting a second we can let all the same cron jobs that were
+      # called at roughly the exact same time finish their write to the
+      # redis cache so that by the time the sleep is over, only one
+      # token is still accurate. I'm hesitant to believe this actually
+      # works, but I can't find any flaws in the logic at the current
+      # moment, and I also believe this is what is keep the EmailQueue
+      # stable which seems to have no duplicate sending problems.
+      if RaceBlock.client.get( @key ) == token
+        RaceBlock.client.expire( @key, expire.is_a?(Integer) ? expire : 60)
+        logger.info("Running block") if debug
+
+        r = yield
+
+        # I have lots of internal debates on whether I should full
+        # delete the key here or still let it sit for a few seconds
+        RaceBlock.client.expire(@key, desync_tokens ? 10 : 3)
+
+        RaceBlock.client.del(@key) if expire_immediately
+
+        return r
+      else
+        # Token out of sync
+        logger.info('Token out of sync') if debug
+      end
+    else
+      # Token already exists
+      logger.info('Token already exists') if debug
+    end
+  end
+
 end
--- spec/race_block_spec.rb
+++ spec/race_block_spec.rb
@@ -1,11 +1,103 @@
 # frozen_string_literal: true
 
+require 'thwait'
+
+RSpec::Matchers.define :one_of do |arr|
+  match { |actual| arr.include?(actual) }
+end
+
+# RSpec::Matchers.define :starts_with do |str|
+#   match { |actual| actual.start_with?(str) }
+# end
+
 RSpec.describe RaceBlock do
+
+  before do
+    ENV['RACK_ENV'] = 'test'
+  end
+
+  before(:each) do |example|
+    @key = example.description
+  end
+
+  after(:each) do
+    RaceBlock.reset(@key)
+  end
+
   it "has a version number" do
     expect(RaceBlock::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "pings redis" do
+    expect(RaceBlock.client.ping).to eq('PONG')
   end
+
+  it "writes to redis" do
+    time = Time.now.to_s
+    expect(RaceBlock.client.set('writes to redis', time)).to eq('OK')
+  end
+
+  it "handles a failed redis connection", reload: true do
+    expect(RaceBlock.logger).to receive(:error)
+    ENV['REDIS_HOST'] = 'google.com'
+    RaceBlock.client(true)
+    # Reset so redis is working again
+    ENV['REDIS_HOST'] = nil
+    RaceBlock.client(true)
+  end
+
+  it "reads what it wrote from redis" do
+    time = Time.now.to_s
+    RaceBlock.client.set("reads what it wrote from redis", time)
+    expect(RaceBlock.client.get("reads what it wrote from redis")).to eq (time)
+  end
+
+  it "has a consistent key" do
+    time = Time.now.to_s
+    expect(RaceBlock.key(time)).to eq("race_block_#{time}")
+  end
+
+  it "fails if no key is provided" do
+    expect { RaceBlock.start('') }.to raise_error('A key must be provided to start a RaceBlock')
+  end
+
+  it "returns it's yield and expire immediately" do
+    returned_value = RaceBlock.start(@key, {expire_immediately: true}) do
+      "yield_returned"
+    end
+
+    expect(returned_value).to eq("yield_returned")
+  end
+
+  it "only runs once" do
+    dbl = double("dbl")
+    expect(dbl).to receive(:log).once
+    expect(RaceBlock.logger).to receive(:info).with('Running block').once
+    expect(RaceBlock.logger).to receive(:info).with(one_of(['Token already exists', 'Token out of sync'])).exactly(2).times
+    threads = (0..2).map do
+      Thread.start do
+        RaceBlock.start(@key, debug: true) do
+          dbl.log
+        end
+      end
+    end
+    ThreadsWait.all_waits threads
+  end
+
+  it "doesn't always run the first call" do
+    dbl = double("dbl")
+    expect(dbl).to receive(:log).once
+    allow(RaceBlock.logger).to receive(:info).with('Token already exists')
+    expect(RaceBlock.logger).to receive(:info).with('Running block').once
+    expect(RaceBlock.logger).to receive(:info).with('Token out of sync').at_least(1).times
+    threads = (0..99).map do
+      Thread.start do
+        RaceBlock.start(@key, debug: true, desync_tokens: true) do
+          dbl.log
+        end
+      end
+    end
+    ThreadsWait.all_waits threads
+  end
+
 end
